f9106351ddb0c536f4e81ff1d0976bdcdbb80364
Serenity <84424454+Serenity482@users.noreply.github.com>
2022-10-10 10:12:17 -0700

    Edited everything

---
+++ main.js
@@ -0,0 +1,11 @@
+var Spaces = []
+
+Spaces[0] = document.getElementById("A1")
+Spaces[1] = document.getElementById("A2")
+Spaces[2] = document.getElementById("A3")
+Spaces[3] = document.getElementById("B1")
+Spaces[4] = document.getElementById("B2")
+Spaces[5] = document.getElementById("B3")
+Spaces[6] = document.getElementById("C1")
+Spaces[7] = document.getElementById("C2")
+Spaces[8] = document.getElementById("C3")
